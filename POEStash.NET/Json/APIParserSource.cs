8483a3c6c1eb89b8b1c05edd46816e1ee16500bb
Andreé Hallengren <andree.hallengren@gmail.com>
2017-04-16 01:55:59 +0200

    Made JsonCache a static cache

--- POEStash.NET/Json/APIParserSource.cs
+++ POEStash.NET/Json/APIParserSource.cs
@@ -7,28 +7,26 @@ namespace POEStash
     {
         private const string API_URL = "http://www.pathofexile.com/api/public-stash-tabs?id=";
         private readonly string id;
-        private readonly JsonCache cache;
 
         public APIJsonProvider(string id)
         {
             this.id = id;
-            this.cache = new JsonCache();
         }
 
         public async Task<string> GetJson()
         {
             string json = string.Empty;
 
-            if (cache.HasJsonForKey(id))
+            if (JsonCache.HasJsonForKey(id))
             {
-                json = await cache.LoadJsonFromCache(id);
+                json = await JsonCache.LoadJsonFromCache(id);
                 return json;
             }
             
             using (var httpClient = new HttpClient())
             {
                 json = await httpClient.GetStringAsync(API_URL + id);
-                await cache.Cache(id, json);
+                await JsonCache.Cache(id, json);
             }
 
             return json;
